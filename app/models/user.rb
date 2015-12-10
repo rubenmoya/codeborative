6890021a3c60511bd9bbc031ea1c4fed7b21efea
rubenmoya <rmoyarodriguez@gmail.com>
2015-12-10 17:03:42 +0100

    Edit views

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,10 @@ class User < ActiveRecord::Base
     active_friends | passive_friends
   end
 
+  def are_friends user_id
+    self.active_friends.where('user_id = ?  || friend_id = ?', user_id, user_id)
+  end
+
   def last_conversation
     last_conversation = Message.where(user_id: self.id).order('created_at').last
 
